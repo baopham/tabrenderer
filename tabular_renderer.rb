81cfdaba5466ad792fb5b8bb396caae8895b1de4
Bao Pham <gbaopham@gmail.com>
2014-05-14 17:59:39 -0700

    Add error pages

--- tabular_renderer.rb
+++ tabular_renderer.rb
@@ -39,3 +39,24 @@ get '/privacy' do
   @page = 'privacy'
   haml :privacy
 end
+
+get '/error' do
+  @error = params['err']
+  if @error
+    @title = "Error | #{app_title}"
+    @page = 'error'
+    haml :error
+  else
+    raise not_found
+  end
+end
+
+not_found do
+  @title = "404 Error | #{app_title}"
+  @page = '404'
+  haml :'404'
+end
+
+error do
+  redirect to("/error?err=#{env['sinatra.error'].name}")
+end
