cba63efa2ddba06dcc7bbf6404f6354800b53c60
Bao Pham <gbaopham@gmail.com>
2014-05-23 17:39:52 -0700

    Avoid unescaped single quotes in textarea

--- public/javascript/angular/app.js
+++ public/javascript/angular/app.js
@@ -86,3 +86,17 @@ app.directive("ngFileSelect",function() {
   
 });
 
+// Directive to set the modal value from the input/textarea field
+app.directive('ngInitial', function() {
+  return {
+    restrict: 'A',
+    controller: [
+      '$scope', '$element', '$attrs', '$parse', function($scope, $element, $attrs, $parse) {
+        var val = $attrs.value || $element.text();
+        var getter = $parse($attrs.ngModel);
+        var setter = getter.assign;
+        setter($scope, val);
+      }
+    ]
+  };
+});
